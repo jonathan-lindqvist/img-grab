b40312f219ffa5632389e480d6e5e3007ba4558b
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2022-12-15 21:04:50 +0100

    Add default directory

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+import os
+
 print("What directory should the files be saved to? (Default directory is documents")
 dir = input()
+
+if(len(dir) == 0):
+    if os.name == "nt":
+        dir = "C:\\Documents\\"
+    else:
+        dir = "/home/Documents/"
