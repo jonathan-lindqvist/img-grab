c5b168ebb03bd0094219485491a128d5353ef264
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2022-12-15 22:56:59 +0100

    Start next feature, document coming features

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import webbrowser
+#import pyscreenshot as sc
 
 print("What directory should the files be saved to? (Default directory is documents")
 dir = input()
@@ -9,13 +11,26 @@ if(len(dir) == 0):
     else:
         dir = "/home/Documents/"
 
+
+attack_path = os.path.join("ValorantAI", "Attack")
+defense_path = os.path.join("ValorantAI", "Defense")
 # Temp solution, does not use dir
 try:
     os.mkdir("ValorantAI")
-    attack_path = os.path.join("ValorantAI", "Attack")
-    defense_path = os.path.join("ValorantAI", "Defense")
     os.mkdir(attack_path)
     os.mkdir(defense_path)
 except FileExistsError:
     pass # Just use existing dirs
 
+# Next feature
+# open browser
+# Screenshot
+# Screenshot specific area
+# Save to dir
+# Open demo 
+# play demo
+# screenshot rounds
+# save into corresponding folder
+webbrowser.open('http://www.python.org', autoraise=False)
+#im = sc.grab()
+#im.show
