37e121bb3de33417df0a05d1bb4cdbe9f18d7ca0
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2022-12-16 21:57:44 +0100

    Save screenshot to dir

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
-from selenium.webdriver import Firefox
-from selenium.webdriver.firefox.options import Options
-
+from selenium import webdriver
+import shutil
 print("What directory should the files be saved to? (Default directory is documents")
 dir = input()
 
@@ -16,22 +15,26 @@ attack_path = os.path.join("ValorantAI", "Attack")
 defense_path = os.path.join("ValorantAI", "Defense")
 # Temp solution, does not use dir
 try:
-    os.mkdir("ValorantAI")
-    os.mkdir(attack_path)
-    os.mkdir(defense_path)
+    access = 0o777
+    os.mkdir("ValorantAI", mode=access)
+    os.mkdir(attack_path, mode=access)
+    os.mkdir(defense_path, mode=access)
 except FileExistsError:
     pass # Just use existing dirs
 
 # Next feature
-# open browser
-# Screenshot
-# Screenshot specific area
-# Save to dir
+# open browser [DONE]
+# Screenshot [DONE]
+# Save to dir [DONE]
 # Open demo 
 # play demo
 # screenshot rounds
+# Screenshot Crop image
 # save into corresponding folder
-browser = Firefox()
-browser.get("https://google.com")
+# loop
+driver = webdriver.Firefox()
+driver.get("https://google.com")
 
-
+driver.save_screenshot("test.png")
+shutil.move("test.png", os.path.join(attack_path,"test.png"))
+print("screenshot taken")
